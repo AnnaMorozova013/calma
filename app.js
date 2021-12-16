63766d3f105aacf6038decd161a7661be3abc823
Anna Morozova <a.morozova.nrw@gmail.com>
2021-12-16 18:50:47 +0100

    deployment

--- app.js
+++ app.js
@@ -45,7 +45,17 @@ app.use('/', auth)
 const data = require('./routes/data')
 app.use('/', data)
 
+//for deployment
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
 
 module.exports = app;
